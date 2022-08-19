fe608a6e807e7ee194e0f6c5a9a3ab5d5b6d782e
AYATSUKO2 <x10cd402PRaya0314@gmail.com>
2022-08-19 10:59:41 +0900

    STATIC_ROOTの変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,10 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media')
 )
 # for /static/root/favicon.ico    
 WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static')
