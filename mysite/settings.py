918b5fe5c36b97cd35a2a6181c97499e66ec86c0
AYATSUKO2 <x10cd402PRaya0314@gmail.com>
2022-08-22 15:28:46 +0900

    MEDIA_URL変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -145,7 +145,7 @@ WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = (
     os.path.join(BASE_DIR , 'static/img/'),
 )
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/img/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
