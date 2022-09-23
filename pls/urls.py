970541a90aa1684f0228eb19600436a55a698b8d
user.email <0kq@0kqnet.work>
2022-09-23 19:06:40 +0900

    Revert "ログイン/登録の実装"
    This reverts commit d4a5031dd2bfd04620e84c451fad1328ad85eef6.

--- pls/urls.py
+++ pls/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.log_in, name='log_in'),
-    path('login', views.account_login, name="login"),
-    path('register', views.create_account, name="register"),
-    path("home",views.home, name="home")
 ]
--- pls/views.py
+++ pls/views.py
@@ -1,51 +1,6 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
-from django.views.generic import CreateView
-from pls.forms import LoginForm, UserCreateForm
-from django.views import View
+from django.shortcuts import render
 
 # Create your views here.
 
-def log_in(req):
+def log_in(req) :
     return render(req, 'pls/log_in.html', {})
-
-def home(req):
-    return render(req, "pls/home.html", {})
-
-class Create_account(CreateView):
-    def post(self, request, *args, **kwargs):
-        form = UserCreateForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            #フォームから'username'を読み取る
-            username = form.cleaned_data.get('username')
-            #フォームから'password1'を読み取る
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('/home')
-        return render(request, 'pls/register.html', {'form': form,})
-
-    def get(self, request, *args, **kwargs):
-        form = UserCreateForm(request.POST)
-        return  render(request, 'pls/register.html', {'form': form,})
-
-create_account = Create_account.as_view()
-
-#ログイン機能
-class Account_login(View):
-    def post(self, request, *arg, **kwargs):
-        form = LoginForm(data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            user = User.objects.get(username=username)
-            login(request, user)
-            return redirect('/home')
-        return render(request, 'pls/login.html', {'form': form,})
-
-    def get(self, request, *args, **kwargs):
-        form = LoginForm(request.POST)
-        return render(request, 'pls/login.html', {'form': form,})
-
-account_login = Account_login.as_view()
